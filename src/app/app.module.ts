2f60829471eab583f2dfdce18fd72603ca927fb2
Chakit Arora <arorachakit@gmail.com>
2020-07-25 10:02:17 +0530

    improved-frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {PostsModule} from './posts/posts.module'
+import { AppRoutingModule } from './app-routing.module';
+import { AuthInterceptor } from './auth/auth-interceptor';
+import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    PostsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+               {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}],
+  bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent]
 })
 export class AppModule { }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Post} from '../post.model'
+import {PostsService} from '../posts.service'
+import {Subscription} from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+
+  // posts = [
+  //   {title: 'First Post', content: "This is first post"},
+  //   {title: 'Second Post', content: "This is second post"},
+  //   {title: 'Third Post', content: "This is third post"}
+  // ]
+
+  posts: Post[] =[]
+  isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 2;
+  currentPage = 1
+  pageSizeOptions = [1, 2, 5, 10]
+  userIsAuthenticated = false;
+  userId: string;
+  private postsSub : Subscription;
+  private authStatusSub: Subscription
+
+  constructor(private postsService: PostsService, private authService: AuthService){}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.userId = this.authService.getUserId();
+     this.postsSub = this.postsService.getPostUpdateListner().subscribe(
+      (postData:{posts: Post[], postCount: number})=> {
+        this.isLoading = false;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
+      }
+    )
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(isAuthenticated =>{
+      this.userIsAuthenticated = isAuthenticated;
+      this.userId = this.authService.getUserId();
+    });
+  }
+
+  onChangePage(pageData : PageEvent){
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex+1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
+
+  onDelete(postId: string){
+    this.isLoading = true;
+    this.postsService.deletePost(postId).subscribe(()=>{
+      this.postsService.getPosts(this.postsPerPage, this.currentPage)
+    }, ()=>{
+      this.isLoading = false;
+    } );
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe()
+    this.authStatusSub.unsubscribe();
+  }
+
+}
